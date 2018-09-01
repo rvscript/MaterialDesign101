11ac9c50f0e7f1d7326a86e061292fd2503a931f
Discovery Dev <rv193@msn.com>
2018-09-01 18:13:40 -0400

    colorPrimary, color Index, MD color contrast and views added

--- app/src/main/java/com/example/ga_mlsdiscovery/materialdesign101/MainActivity.java
+++ app/src/main/java/com/example/ga_mlsdiscovery/materialdesign101/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.ga_mlsdiscovery.materialdesign101;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int wHeight = displayMetrics.heightPixels;
+        int wWidth = displayMetrics.widthPixels;
+
+        //check if we're running on Android 5.0 or higher
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+
+        } else { //Below Lollipop verions 21
+
+        }
+
+        mTextView = findViewById(R.id.tv_text);
+        mTextView.setText("window Height = "+wHeight +
+                " AND window Width = "+wWidth);
+
     }
 }
